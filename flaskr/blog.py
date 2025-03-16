a53a707b808b3185aed4e09dd54e71ce7b1473fc
sofib <sofi.blova@gmail.com>
2025-03-16 13:49:10 +0100

    fix: correct database call in get_post

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -47,7 +47,7 @@ def create():
 
 
 def get_post(id, check_author=True):
-    post = get_db.execute(
+    post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
@@ -88,10 +88,10 @@ def update(id):
             db.commit()
             return redirect(url_for('blog.index'))
         
-    return render_template('blog/create.html', post=post)
+    return render_template('blog/update.html', post=post)
 
 
-@bp.route('/<int:id>/delete', methods=("POST"))
+@bp.route('/<int:id>/delete', methods=["POST"])
 @login_required
 def delete(id):
     get_post(id)
