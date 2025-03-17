01aae61ff89e0df2a1326001a7b996950297f26d
sofib <sofi.blova@gmail.com>
2025-03-18 00:42:37 +0100

    fix: correct SQL syntax in update post query and add testing framework configuration

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -81,9 +81,9 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?)'
+                'UPDATE post SET title = ?, body = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (title, body, id,)
             )
             db.commit()
             return redirect(url_for('blog.index'))
